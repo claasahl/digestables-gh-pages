7b5ce694570fe40c7229969df00721218c25fccf
Claas Ahlrichs <claasahl@gmail.com>
2018-04-17 06:45:37 +0200

    added link and button for testing

--- pom-xml/src/App.tsx
+++ pom-xml/src/App.tsx
@@ -5,6 +5,7 @@ import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
+    const sample = JSON.stringify({ test: "testttt"});
     return (
       <div className="App">
         <header className="App-header">
@@ -14,8 +15,16 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button onClick={this.handleClick}>click me</button>
+        <a href={'data:text/plain;charset=utf-8,' + encodeURIComponent(sample)} download="pom.xml">recipe</a>
+
       </div>
     );
+  }
+
+  private handleClick() {
+    console.log("clicked");
+
   }
 }
 
