724c8725dafbde7881ab648a20536d750b65858b
Claas Ahlrichs <claasahl@gmail.com>
2018-04-20 06:52:49 +0200

    added husky

--- pom-xml/src/App.tsx
+++ pom-xml/src/App.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 class App extends React.Component {
   public render() {
-    const sample = JSON.stringify({ test: "testttt"});
+    const sample = JSON.stringify({ test: "testttt" });
     return (
       <div className="App">
         <header className="App-header">
@@ -15,8 +15,12 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a href={'data:text/plain;charset=utf-8,' + encodeURIComponent(sample)} download="pom.xml">recipe</a>
-
+        <a
+          href={"data:text/plain;charset=utf-8," + encodeURIComponent(sample)}
+          download="pom.xml"
+        >
+          recipe
+        </a>
       </div>
     );
   }
