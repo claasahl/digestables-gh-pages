6745e8525a6138f4d71252f245015177ebbb1367
Claas Ahlrichs <claasahl@gmail.com>
2018-04-21 14:09:35 +0200

    attempts to load sample XML

--- pom-xml/src/App.tsx
+++ pom-xml/src/App.tsx
@@ -3,9 +3,12 @@ import "./App.css";
 
 import logo from "./logo.svg";
 
+import sampleXml from "./recipes/sample-recipe.xml";
+
 class App extends React.Component {
   public render() {
     const sample = JSON.stringify({ test: "testttt" });
+    const sample2 = JSON.stringify(sampleXml);
     return (
       <div className="App">
         <header className="App-header">
@@ -13,7 +16,7 @@ class App extends React.Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          To get started, edit <code>{sample2}</code> and save to reload.
         </p>
         <a
           href={"data:text/plain;charset=utf-8," + encodeURIComponent(sample)}
