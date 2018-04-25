eee95cfb422ddce89a1ba1c51ae9480fbafa5621
Claas Ahlrichs <claasahl@gmail.com>
2018-04-25 06:45:08 +0200

    parsing of hardcoded sample XML

--- pom-xml/src/App.tsx
+++ pom-xml/src/App.tsx
@@ -1,14 +1,31 @@
 import * as React from "react";
+import { parseString } from "xml2js";
 import "./App.css";
 
 import logo from "./logo.svg";
 
-import sampleXml from "./recipes/sample-recipe.xml";
-
 class App extends React.Component {
   public render() {
+    const data = `<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+    <groupId>org.github.claasahl</groupId>
+    <artifactId>maven-starter-parent</artifactId>
+    <packaging>pom</packaging>
+    <version>0.1.0</version>
+    <modules>
+      <module>maven-starter</module>
+      <module>maven-starter-example</module>
+    </modules>
+  </project>
+  `;
+    let sample3;
+    parseString(data, (err, result) => {
+      sample3 = result;
+    });
+
     const sample = JSON.stringify({ test: "testttt" });
-    const sample2 = JSON.stringify(sampleXml);
+    const sample2 = JSON.stringify(sample3);
     return (
       <div className="App">
         <header className="App-header">
