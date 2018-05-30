72ba6b0fa571ec1988c83fd1c4fa8413b9a29567
Claas Ahlrichs <claasahl@gmail.com>
2018-05-30 06:53:56 +0200

    drafted component for managing ingredients

---
+++ pom-xml/src/Recipe.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./Recipe.css";
+
+class Recipe extends React.Component {
+  public render() {
+    return "I am a recipe!";
+  }
+}
+
+export default Recipe;
