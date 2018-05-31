f41c59fe54fa221dbb1d62efc7e7d46a50f5f994
Claas Ahlrichs <claasahl@gmail.com>
2018-05-31 06:54:09 +0200

    drafted add/delete functions

--- pom-xml/src/Recipe.tsx
+++ pom-xml/src/Recipe.tsx
@@ -1,9 +1,58 @@
 import * as React from "react";
 import "./Recipe.css";
 
-class Recipe extends React.Component {
+interface IIngredient {
+  name: string;
+  xml: string;
+}
+
+interface IRecipeState {
+  ingredients: IIngredient[];
+}
+
+class Recipe extends React.Component<any, IRecipeState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      ingredients: []
+    };
+    this.handleSampleAdd = this.handleSampleAdd.bind(this);
+    this.handleSampleRemove = this.handleSampleRemove.bind(this);
+  }
   public render() {
-    return "I am a recipe!";
+    return (
+      <div>
+        <button onClick={this.handleSampleAdd}>add</button>
+        <button onClick={this.handleSampleRemove}>remove</button>
+        {this.state.ingredients.map(({ name }) => <div key={name}>{name}</div>)}
+      </div>
+    );
+  }
+
+  private handleSampleAdd(event: React.MouseEvent<HTMLButtonElement>): void {
+    event.preventDefault();
+    this.add({ name: "test", xml: "<data></data>" });
+  }
+
+  private handleSampleRemove(event: React.MouseEvent<HTMLButtonElement>): void {
+    event.preventDefault();
+    this.remove({ name: "test", xml: "<data></data>" });
+  }
+
+  private add(ingredient: IIngredient): void {
+    this.setState(state => ({
+      ingredients: [...state.ingredients, ingredient]
+    }));
+  }
+
+  private remove(ingredient: IIngredient): void {
+    this.setState(state => {
+      const ingredients = [...state.ingredients];
+      const index = ingredients.findIndex(value => value === ingredient);
+      return {
+        ingredients: index >= 0 ? ingredients.splice(index, 1) : ingredients
+      };
+    });
   }
 }
 
