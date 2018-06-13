65da80961c7d55d6a7f0f83c555e4d7e79c0bee4
Claas Ahlrichs <claasahl@gmail.com>
2018-06-13 06:41:31 +0200

    added module which lists all available sample  files

---
+++ pom-xml/src/ingredients/Ingredients.tsx
@@ -0,0 +1,8 @@
+import Sample1 from "./data/sample1.xml";
+import Sample2 from "./data/sample2.xml";
+
+const ingredients = [
+  { name: "sample1", url: Sample1 },
+  { name: "sample2", url: Sample2 }
+];
+export default ingredients;
