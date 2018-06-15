5f3f53e04ce7dfd75199f052b29151427026750e
Claas Ahlrichs <claasahl@gmail.com>
2018-06-15 06:42:57 +0200

    removed sample from App component

--- pom-xml/src/App.tsx
+++ pom-xml/src/App.tsx
@@ -3,38 +3,19 @@ import * as React from "react";
 import "./App.css";
 
 import DownshiftSample from "./DownshiftSample";
-import { mergeIngredients } from "./ingredients/IngredientsMerger";
 import RecipeComposer from "./RecipeComposer";
-
-import { ingredients } from "./ingredients/Ingredients";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
 library.add(fas);
 
-class App extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      sample: ""
-    };
-  }
-
-  public componentWillMount() {
-    const sample = mergeIngredients([ingredients[0]]);
-    sample.then(result => {
-      this.setState((state: any) => ({
-        sample: result
-      }));
-    });
-  }
+class App extends React.Component {
   public render() {
     return (
       <div className="container grid-wrapper">
         <DownshiftSample />
         <RecipeComposer />
-        {JSON.stringify(this.state.sample)}
       </div>
     );
   }
