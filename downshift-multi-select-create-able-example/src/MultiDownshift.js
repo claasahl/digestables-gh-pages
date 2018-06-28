0b362515d0f8898aab3afb96f65449df42255997
Claas Ahlrichs <claasahl@gmail.com>
2018-06-28 06:50:35 +0200

    imported downshift-multi-select-create-able-example

---
+++ downshift-multi-select-create-able-example/src/MultiDownshift.js
@@ -0,0 +1,210 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import glamorous, { Div } from "glamorous";
+import { css } from "glamor";
+import matchSorter from "match-sorter";
+import starWarsNames from "starwars-names";
+import Downshift from "downshift";
+import AutosizeInput from "react-input-autosize";
+import {
+  Label,
+  Menu,
+  ControllerButton,
+  Input,
+  Item,
+  ArrowIcon,
+  XIcon,
+  InputValueWrapper
+} from "./components";
+import TagValue from "./TagValue";
+
+const inputBoxCss = css({
+  border: "none",
+  outline: "none",
+  cursor: "inherit",
+  backgroundColor: "transparent",
+  fontSize: "14px"
+});
+
+class MultiDownshift extends React.Component {
+  state = { inputValue: "" };
+
+  handleStateChange = (changes, downshiftStateAndHelpers) => {
+    if (!downshiftStateAndHelpers.isOpen) {
+      this.setState({ inputValue: "" });
+    }
+
+    if (this.props.onChangedState) {
+      this.props.onChangedState(changes, downshiftStateAndHelpers);
+    }
+  };
+
+  onInputChange = event => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  onTagBlur = (e, item) => {
+    const { onItemChanged } = this.props;
+    if (onItemChanged) {
+      onItemChanged(item);
+    }
+  };
+
+  onRemoveTag = item => {
+    this.props.onRemoveItem(item);
+  };
+
+  onInputKeyDown = event => {
+    const currentValue = event.target.value;
+    switch (event.keyCode) {
+      case 8: // backspace
+        if (!currentValue) {
+          event.preventDefault();
+          this.popValue();
+        }
+        return;
+      case 9: // tab
+        if (this.state.inputValue) {
+          this.props.onChange(this.state.inputValue);
+          this.setState({ inputValue: "" });
+        }
+
+        event.preventDefault();
+        event.stopPropagation();
+        return;
+      case 46: // backspace
+        if (!this.state.inputValue && this.props.deleteRemoves) {
+          event.preventDefault();
+          this.popValue();
+        }
+        return;
+      case 188: // comma
+        if (!this.state.inputValue) {
+          event.preventDefault();
+        } else {
+          this.props.onChange(this.state.inputValue);
+          this.setState({ inputValue: "" });
+        }
+        break;
+      default:
+        return;
+    }
+    event.preventDefault();
+  };
+
+  popValue() {
+    const { selectedItem, onRemoveItem } = this.props;
+    if (onRemoveItem) {
+      onRemoveItem({
+        value: selectedItem[selectedItem.length - 1],
+        index: selectedItem.length - 1
+      });
+    }
+  }
+
+  onWrapperClick = e => {
+    if (this._inputWrapper === e.target || this._input === e.target) {
+      this.focusOnInput();
+      e.stopPropagation();
+      e.preventDefault();
+    }
+  };
+
+  focusOnInput() {
+    this._input.focus();
+    if (typeof this._input.getInput === "function") {
+      this._input.getInput().focus();
+    }
+  }
+
+  inputRef = c => {
+    this._input = c;
+  };
+
+  inputWrapperRef = c => {
+    this._inputWrapper = c;
+  };
+
+  render() {
+    const { itemToString, items, ...rest } = this.props;
+    return (
+      <Downshift
+        onStateChange={this.handleStateChange}
+        itemToString={itemToString}
+        {...rest}
+      >
+        {({
+          getLabelProps,
+          getInputProps,
+          getButtonProps,
+          getItemProps,
+          isOpen,
+          toggleMenu,
+          clearSelection,
+          selectedItem,
+          highlightedIndex
+        }) => {
+          const _inputProps = getInputProps({
+            value: this.state.inputValue,
+            ref: this.inputRef,
+            inputClassName: inputBoxCss.toString(),
+            onChange: this.onInputChange,
+            onKeyDown: this.onInputKeyDown
+          });
+
+          const tagItems = selectedItem.map((item, index) => {
+            return { value: item, index };
+          });
+
+          return (
+            <div className={css({ width: 300, margin: "auto" })}>
+              <Label {...getLabelProps()}>
+                Find or create a Star Wars character
+              </Label>
+              <Div position="relative">
+                <InputValueWrapper
+                  innerRef={this.inputWrapperRef}
+                  onClick={this.onWrapperClick}
+                  tabIndex="-1"
+                >
+                  {tagItems.map(tag => (
+                    <TagValue
+                      key={`Tag-${tag.index}`}
+                      onBlur={this.onTagBlur}
+                      onRemove={this.onRemoveTag}
+                      tag={tag}
+                    />
+                  ))}
+                  <AutosizeInput {..._inputProps} />
+                </InputValueWrapper>
+
+                <ControllerButton {...getButtonProps()}>
+                  <ArrowIcon isOpen={isOpen} />
+                </ControllerButton>
+              </Div>
+              {!isOpen ? null : (
+                <Menu>
+                  {items.map((item, index) => (
+                    <Item
+                      key={`item-${index}`}
+                      {...getItemProps({
+                        item,
+                        index,
+                        isActive: highlightedIndex === index,
+                        isSelected: selectedItem === item
+                      })}
+                    >
+                      {item}
+                    </Item>
+                  ))}
+                </Menu>
+              )}
+            </div>
+          );
+        }}
+      </Downshift>
+    );
+  }
+}
+
+export default MultiDownshift;
---
+++ downshift-multi-select-create-able-example/src/index.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import glamorous, { Div } from "glamorous";
+import { css } from "glamor";
+import matchSorter from "match-sorter";
+import starWarsNames from "starwars-names";
+import Downshift from "downshift";
+import {
+  Label,
+  Menu,
+  ControllerButton,
+  Input,
+  Item,
+  ArrowIcon,
+  XIcon
+} from "./components";
+import MultiDownshift from "./MultiDownshift";
+
+class App extends React.Component {
+  allItems = starWarsNames.all;
+  state = { items: this.allItems, selectedItems: [] };
+  handleStateChange = (changes, downshiftState) => {
+    if (changes.hasOwnProperty("inputValue")) {
+      this.setState({ items: this.getItems(changes.inputValue) });
+    }
+    // handle stuff here if you need to
+    // this is especially useful if you need
+    // to controll some of the internal state yourself
+  };
+
+  handleChange = (selectedItem, downshiftState) => {
+    this.setState({ items: this.allItems });
+  };
+
+  getItems = value => {
+    return value ? matchSorter(this.allItems, value) : this.allItems;
+  };
+
+  onItemAdd = selectedItem => {
+    this.setState({
+      selectedItems: [...this.state.selectedItems, selectedItem]
+    });
+  };
+
+  onRemoveItem = item => {
+    const copy = [...this.state.selectedItems];
+    copy.splice(item.index, 1);
+    this.setState({ selectedItems: copy });
+  };
+
+  onItemChanged = item => {
+    const copy = [...this.state.selectedItems];
+    copy.splice(item.index, 1, item.value);
+    this.setState({ selectedItems: copy });
+  };
+
+  itemToString(i) {
+    return i ? i.name : "";
+  }
+
+  render() {
+    return (
+      <Div
+        css={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          textAlign: "center"
+        }}
+      >
+        <h2>Multi select creatable example</h2>
+        <MultiDownshift
+          selectedItem={this.state.selectedItems}
+          onChangedState={this.handleStateChange}
+          onChange={this.onItemAdd}
+          onItemChanged={this.onItemChanged}
+          onRemoveItem={this.onRemoveItem}
+          items={this.state.items}
+          itemToString={this.itemToString}
+        />
+      </Div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
