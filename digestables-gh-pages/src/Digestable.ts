03c33d7210fa5a9b58e7a3ea9ced372fff1997be
Claas Ahlrichs <claasahl@gmail.com>
2018-08-01 06:48:21 +0200

    moved to project to root of git repository

--- digestables-gh-pages/src/Digestable.ts
+++
@@ -1,7 +0,0 @@
-export interface IDigestable {
-  baseURL: URL;
-  name: string;
-  files: string[];
-}
-
-export default IDigestable;
---
+++ public/digestables/hello-world/src/main/java/org/github/claasahl/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.github.claasahl;
+
+public class HelloWorld {
+    /**
+     * Some documentation.
+     * 
+     * @param args
+     *            a bunch of arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
